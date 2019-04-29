9d2fc6cd25bbafc1032c3fb3b21324fc9ec7886b
nickygbg <50113362+nickygbg@users.noreply.github.com>
2019-04-29 17:39:46 +0100

    Add files via upload

---
+++ Demo.js
@@ -0,0 +1,49 @@
+/*for(i = 44 ; i < 67 ; i++ ) 
+   {console.log(i) ;}*/
+//Log 44-66 to the console    
+
+   /*console.log("Wamp".toUpperCase())*/
+
+   /*console. log(Math.floor(Math.random()*10));*/
+
+
+   let space1 = "X"
+   let space2 = "O"
+   let space3 = " "
+   let space4 = "X"
+   let space5 = "X"
+   let space6 = " "
+   let space7 = "O"
+   let space8 = " "
+   let space9 = " "
+
+console.log (`   |   |  `         )  
+console.log (` ${space1} | ${space2} | ${space3} `) 
+console.log (`   |   |`         ) 
+console.log (`-----------`  )
+console.log (`   |   |`       ) 
+console.log (` ${space4} | ${space5} |${space6}`        ) 
+console.log (`   |   |`        ) 
+console.log (`-----------` )
+console.log (`   |   |`         )
+console.log (` ${space7} | ${space8} |${space9}`         )
+console.log (`   |   |`        )
+
+
+
+/*console. log("All Around the world".toUpperCase().charAt(7));*/
+
+/*let i = 10;*/
+/*const i = 10;*/
+/*var i = 10;*/
+
+/*let favouriteDrink = "Coffee";
+console. log(favouriteDrink);
+
+console.log(`My favourite drink is ${favouriteDrink}`);*/
+
+/*let name = "Bill";
+let age = "27";
+let favouritecolour = "blue";
+console.log(`My name is ${name}. I am ${age} and my favourite colour is ${favouritecolour}.`)*/
+
